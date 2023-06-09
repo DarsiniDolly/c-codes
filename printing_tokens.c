0abece480ca2206c4ed0934294cee1c80fc0eaac
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-09 09:47:56 +0530

    Add files via upload

---
+++ printing_tokens.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    printf("sentence to print: %s",s);
+    scanf("%[^\n]",s);
+    s = realloc(s, strlen(s) + 1);
+    int k=0;
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if(s[i]==' '){
+            printf("\n");
+        }
+        printf("%c",s[i]);
+    }
+    return 0;
+
+}

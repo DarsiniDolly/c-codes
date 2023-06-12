fa86adade29352a109e77a0260abf4715946df76
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-12 22:46:31 +0530

    Update simplecalculator.c

--- simplecalculator.c
+++ simplecalculator.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
 
+typedef int (*Operation)(int, int);
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+int processInput(int a, int b, char operator, Operation op) {
+    return op(a, b);
+}
+
 int main() {
-    int num1, num2;
+    int num1, num2, result;
     char operator;
+    char exitKey;
 
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    Operation operation;
 
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator);
+    while (1) {
+        printf("Enter the first number: ");
+        scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+        printf("Enter the operator (+, -, *, /): ");
+        scanf(" %c", &operator);
 
-    switch (operator) {
-        case '+':
-            printf("%d + %d = %d\n", num1, num2, num1 + num2);
-            break;
-        case '-':
-            printf("%d - %d = %d\n", num1, num2, num1 - num2);
-            break;
-        case '*':
-            printf("%d * %d = %d\n", num1, num2, num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0) {
-                printf("%d / %d = %f\n", num1, num2, (float)num1 / num2);
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-            }
-            break;
-        default:
-            printf("Error: Invalid operator.\n");
+        printf("Enter the second number: ");
+        scanf("%d", &num2);
+
+
+        // Assign the appropriate function pointer based on the operator
+        switch (operator) {
+            case '+':
+                operation = add;
+                break;
+            case '-':
+                operation = subtract;
+                break;
+            case '*':
+                operation = multiply;
+                break;
+            case '/':
+                operation = divide;
+                break;
+            default:
+                printf("Invalid operator! Please try again.\n");
+                continue;  // Restart the loop if the operator is invalid
+        }
+
+        // Perform the operation and display the result
+        result = processInput(num1, num2, operator, operation);
+        printf("Result: %d\n\n", result);
+
+        printf("Press 'q' to exit or any other key to continue...\n");
+        scanf(" %c", &exitKey);
+        if (exitKey == 'q')  // Check if 'q' key is pressed
             break;
     }
 

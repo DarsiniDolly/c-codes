2c3eacb828cc85a102d35b64c1676528fb34e88b
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-16 09:44:48 +0530

    Update students_marks.c

--- students_marks.c
+++ students_marks.c
@@ -7,16 +7,15 @@ struct Student {
     int mark1;
     int mark2;
     int mark3;
-    char gender;
+    char gender[10];
     int total;
     char result;
 } s[100];
 
-int main() {
+void manageStudents() {
     int i, k, rn;
-    char g;
     int pass = 0, fail = 0; 
-    int count = 5; 
+    int count = 3; 
 
     printf("Enter information of students:\n");
 
@@ -32,8 +31,8 @@ int main() {
         scanf("%d", &s[i].mark2);
         printf("Enter mark3: ");
         scanf("%d", &s[i].mark3);
-        printf("Enter gender (M/F): ");
-        scanf(" %c", &s[i].gender); 
+        printf("Enter gender: ");
+        scanf("%s", s[i].gender); 
         s[i].total = s[i].mark1 + s[i].mark2 + s[i].mark3;
 
         // Determine pass/fail
@@ -55,7 +54,7 @@ int main() {
         printf("Mark1: %d\n", s[i].mark1);
         printf("Mark2: %d\n", s[i].mark2);
         printf("Mark3: %d\n", s[i].mark3);
-        printf("Gender: %c\n", s[i].gender);
+        printf("Gender: %s\n", s[i].gender);
         printf("Total: %d\n", s[i].total);
         printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
     }
@@ -64,11 +63,12 @@ int main() {
     printf("Number of students failed: %d\n", fail);
 
 
-    printf("\nEnter the gender  to find the student details: \n");
-    scanf(" %c", &g);
+    printf("\nEnter the gender to find the student details: \n");
+    char gendertochoose[10];
+    scanf("%s", gendertochoose);
 
     for (i = 0; i < count; i++) {
-        if (s[i].gender == g) {
+        if (strcmp(s[i].gender, gendertochoose) == 0) {
             printf("\nName: %s\n", s[i].fname);
             printf("Roll number: %d\n", s[i].roll);
             printf("Marks: %d\n", s[i].total);
@@ -84,83 +84,97 @@ int main() {
     for (i = 0; i < count; i++) {
         if (strcmp(s[i].fname, nameToDelete) == 0) {
             k = i; // Position of the student to delete
-            printf("\nStudent '%s' found at position %d\n", s[k].fname, k + 1);
-
-            // Shift the remaining elements to fill the gap
-            for (int j = k; j < count - 1; j++) {
-                s[j] = s[j + 1];
-            }
-
-            count--; // Decrease the count of students
-            printf("Student '%s' deleted successfully!\n", nameToDelete);
+            printf("\nName: %s\n", s[i].fname);
+            printf("Roll number: %d\n", s[i].roll);
+            printf("Marks: %d\n", s[i].total);
+            printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
             break;
         }
     }
 
-    printf("\nDisplaying Updated Information:\n\n");
+    if (k != count) {
+        // Shift remaining elements to fill the gap
+        for (i = k; i < count - 1; i++) {
+            strcpy(s[i].fname, s[i + 1].fname);
+            s[i].roll = s[i + 1].roll;
+            s[i].mark1 = s[i + 1].mark1;
+            s[i].mark2 = s[i + 1].mark2;
+            s[i].mark3 = s[i + 1].mark3;
+            strcpy(s[i].gender, s[i + 1].gender);
+            s[i].total = s[i + 1].total;
+            s[i].result = s[i + 1].result;
+        }
 
-    // displaying updated information
+        count--; // Decrease the count of students
+        printf("\nDeleted the student successfully!\n");
+    } else {
+        printf("\nStudent not found!\n");
+    }
+
+    printf("\nUpdated Information:\n\n");
+
+    // Display updated information
     for (i = 0; i < count; i++) {
         printf("\nRoll number: %d\n", i + 1);
         printf("Name: %s\n", s[i].fname);
         printf("Mark1: %d\n", s[i].mark1);
         printf("Mark2: %d\n", s[i].mark2);
         printf("Mark3: %d\n", s[i].mark3);
-        printf("Gender: %c\n", s[i].gender);
+        printf("Gender: %s\n", s[i].gender);
         printf("Total: %d\n", s[i].total);
         printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
     }
 
-    printf("\nEnter the roll number to find the student details: \n");
+    printf("\nEnter the roll number of the student to update marks: \n");
     scanf("%d", &rn);
 
-    for (i = 0; i < 3; i++) {
+    // Find and update student details by roll number
+    for (i = 0; i < count; i++) {
         if (s[i].roll == rn) {
-            k = i; // for finding the position of the student
-            printf("\nRecord found at position no %d \n", k + 1);
-            printf("Name: %s\n", s[k].fname);
-            printf("Roll number: %d\n", s[k].roll);
-            printf("Marks: %d\n", s[k].total);
-            printf("Result: %s\n", s[k].result == 'P' ? "Passed" : "Failed");
+            printf("\nCurrent Details:\n");
+            printf("Name: %s\n", s[i].fname);
+            printf("Roll number: %d\n", s[i].roll);
+            printf("Marks: %d\n", s[i].total);
+            printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
 
-            printf("\nUpdate marks for the student (roll number %d):\n", s[k].roll);
-            printf("Enter new mark1: ");
-            scanf("%d", &s[k].mark1);
+            printf("\nEnter new mark1: ");
+            scanf("%d", &s[i].mark1);
             printf("Enter new mark2: ");
-            scanf("%d", &s[k].mark2);
+            scanf("%d", &s[i].mark2);
             printf("Enter new mark3: ");
-            scanf("%d", &s[k].mark3);
-            s[k].total = s[k].mark1 + s[k].mark2 + s[k].mark3;
+            scanf("%d", &s[i].mark3);
+            s[i].total = s[i].mark1 + s[i].mark2 + s[i].mark3;
 
             // Update pass/fail status
-            if (s[k].total > 170) {
-                s[k].result = 'P';
+            if (s[i].total > 170) {
+                s[i].result = 'P';
             } else {
-                s[k].result = 'F';
+                s[i].result = 'F';
             }
 
-            printf("\nMarks updated successfully!\n");
-            printf("Updated total: %d\n", s[k].total);
-            printf("Updated result: %s\n", s[k].result == 'P' ? "Passed" : "Failed");
+            printf("\nUpdated Details:\n");
+            printf("Name: %s\n", s[i].fname);
+            printf("Roll number: %d\n", s[i].roll);
+            printf("Marks: %d\n", s[i].total);
+            printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
+
             break;
-         }
-      }
-
-      printf("\nupdated student details: \n");
-  
-      int found = 0; // Flag to track if the roll number is found
-
-      for (i = 0; i < count; i++) {
-        if (s[i].roll == rn) {
-           found = 1;
-           k = i; // for finding the position of the student
-           printf("\nRecord found at position no %d \n", k + 1);
-           printf("Name: %s\n", s[k].fname);
-           printf("Roll number: %d\n", s[k].roll);
-           printf("Marks: %d\n", s[k].total);
-           printf("Result: %s\n", s[k].result == 'P' ? "Passed" : "Failed");
         }
-      }
-           
+    }
+
+    printf("\nFinal Information:\n\n");
+
+    // Display final information
+    for (i = 0; i < count; i++) {
+        printf("\nRoll number: %d\n", i + 1);
+        printf("Name: %s\n", s[i].fname);
+        printf("Gender: %s\n", s[i].gender);
+        printf("Total: %d\n", s[i].total);
+        printf("Result: %s\n", s[i].result == 'P' ? "Passed" : "Failed");
+    }
+}
+
+int main() {
+    manageStudents();
     return 0;
 }

62bb8e400c34de915d178f6fe8aa7f4c3d8b4562
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-22 12:19:50 +0530

    Add files via upload

---
+++ 2darray.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[3][3],b[3][3],add[3][3],i,j;
+    for(int i=0;i<3;++i)
+    {
+        for(int j=0;j<3;++j)
+        {
+            printf("Enter a%d%d: ", i + 1, j + 1);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(int i=0;i<3;++i)
+    {
+        for(int j=0;j<3;++j)
+        {
+            printf("Enter b%d%d: ", i + 1, j + 1);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    
+    for(i=0;i<3;++i)
+    {
+        for(j=0;j<3;++j)
+        {
+            add[i][j]= a[i][j] + b[i][j];
+        }
+    }
+    
+     for(int i=0;i<3;++i)
+    {
+        for(int j=0;j<3;++j)
+        {
+            printf("add %d%d :%d", i+1,j+1, add[i][j]);
+            printf("\n");
+        }
+    }
+
+
+    return 0;
+
+}

3dd457ff0a3f7ac01baa2da90ad3e371466a22e7
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-22 10:40:20 +0530

    Update string sorting.c

--- string sorting.c
+++ string sorting.c
@@ -11,12 +11,7 @@ int stringcompare(const char* str1, const char* str2) {
 
         i++;
     }
-    if (str1[i] == '\0' && str2[i] != '\0')
-        return -1;
-    else if (str1[i] != '\0' && str2[i] == '\0')
-        return 1;
-
-    return 0;
+    
 }
 
 void bubbleSort(char** arr, int n) {

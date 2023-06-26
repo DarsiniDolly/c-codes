f7898cb321be53402a8b4b8e4008f0944e2b8d40
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-06-26 14:25:20 +0530

    Add files via upload

---
+++ stringcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// this program is to count the number of words in the given string
+int main()
+{
+    int i,count=0;
+    char arr[50];
+    printf("Enter the string to count the words:");
+    fgets(arr, 50, stdin);
+    for(i=0;arr[i]!='\0';i++)// terminate the loop when it encounters the null character
+    {
+       if(arr[i]==' ')// count the spaces
+       {
+         count++;
+       }
+    }
+    printf("total no of words in the given string:%d\n",count);
+    return 0;
+}

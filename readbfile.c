b0d32854b3cd3004e94c111203214b7cc1a0a2c4
DOLLY <115916555+DarsiniDolly@users.noreply.github.com>
2023-07-03 11:14:20 +0530

    Update readbfile.c

--- readbfile.c
+++ readbfile.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    char n[50],m;
-    int fsize,fsize1,fsize2,fsize3,fsize4;
+    char n[50],m,filename[50];
+    int fsize,fsize1,fsize2,fsize3,fsize4,size,sum,sum1,sum2,sum3,sum4;
     FILE *fptr;
     FILE *ptr1;
     FILE *ptr2;
@@ -20,7 +20,7 @@ int main()
     ptr4 = fopen("bfile4.bin", "wb");
     ptr5 = fopen("bfile5.bin", "wb");
 
-
+    sum = fsize + size;
     if (fptr == NULL)
     {
         printf("Could not open the merged file.\n");
@@ -31,84 +31,101 @@ int main()
         printf("Could not open the output files.\n");
         
     }
-
+     
+    
+    fread(filename, sizeof(char), 5,fptr);
     fread(&n[0], sizeof(char), 1,fptr);
     fread(&fsize, sizeof(int), 1, fptr);
-    fread(n, sizeof(char), fsize,fptr);
+    fread(n, sizeof(char), fsize-1,fptr);
 
-    while ((m = fgetc(fptr)) == fsize )
+    while ((m = fgetc(fptr)) == sum )
     {
         fputc(m, ptr1);
     }
+    
+    fwrite(filename, sizeof(char), 5,ptr1);
     fwrite(&n[0], sizeof(char), 1,ptr1);
     fwrite(&fsize, sizeof(int), 1, ptr1);
-    fwrite(n, sizeof(char), fsize,ptr1);
+    fwrite(n, sizeof(char), fsize-1,ptr1);
+    printf("filename1:%s\n",filename);
     printf("index1:%c\n",n[0]);
     printf("size1:%d\n",fsize);
     printf("the string1:%s\n",n);
     fclose(ptr1);
 
-    
+    fread(filename, sizeof(char), 5,fptr);
+    fread(&n[0],sizeof(char),1,fptr);
     fread(&fsize1, sizeof(int), 1, fptr);
-    fread(n, sizeof(char), fsize,fptr);
+    fread(n, sizeof(char), fsize-1 ,fptr);
 
-    while ((m = fgetc(fptr)) == fsize1)
+    while ((m = fgetc(fptr)) == sum1)
     {
         fputc(m, ptr2);
     }
+    fwrite(filename, sizeof(char), 4,ptr2);
     fwrite(&n[0], sizeof(char), 1,ptr2);
-    fwrite(&fsize, sizeof(int), 1, ptr2);
-    fwrite(n, sizeof(char), fsize,ptr2);
+    fwrite(&fsize , sizeof(int), 1, ptr2);
+    fwrite(n, sizeof(char), fsize-1 ,ptr2);
+    printf("filename2:%s\n",filename);
     printf("index2:%c\n",n[0]);
     printf("size2:%d\n",fsize);
     printf("the string2:%s\n",n);
     fclose(ptr2);
 
-    
+    fread(filename, sizeof(char), 5,fptr);
+    fread(&n[0],sizeof(char),1,fptr);
     fread(&fsize2, sizeof(int), 1, fptr);
-    fread(n, sizeof(char), fsize,fptr);
+    fread(n, sizeof(char), fsize-1,fptr);
    
-    while ((m = fgetc(fptr)) == fsize2)
+    while ((m = fgetc(fptr)) == sum2)
     {
         fputc(m, ptr3);
     }
+    fwrite(filename, sizeof(char), 4,ptr3);
     fwrite(&n[0], sizeof(char), 1,ptr3);
     fwrite(&fsize, sizeof(int), 1, ptr3);
-    fwrite(n, sizeof(char), fsize,ptr3);
+    fwrite(n, sizeof(char), fsize-1,ptr3);
+     printf("filename3:%s\n",filename);
     printf("index3:%c\n",n[0]);
     printf("size3:%d\n",fsize);
     printf("the string3:%s\n",n);
     fclose(ptr3);
 
 
-    
+    fread(filename, sizeof(char), 5,fptr);
+    fread(&n[0],sizeof(char),1,fptr);
     fread(&fsize3, sizeof(int), 1, fptr);
-    fread(n, sizeof(char), fsize,fptr);
+    fread(n, sizeof(char), fsize-1,fptr);
 
-    while ((m = fgetc(fptr)) == fsize3)
+    while ((m = fgetc(fptr)) == sum3)
     {
         fputc(m, ptr4);
     }
+    fwrite(filename, sizeof(char),4,ptr4);
     fwrite(&n[0], sizeof(char), 1,ptr4);
     fwrite(&fsize, sizeof(int), 1, ptr4);
-    fwrite(n, sizeof(char), fsize,ptr4);
+    fwrite(n, sizeof(char), fsize-1,ptr4);
+    printf("filename4:%s\n",filename);
     printf("index4:%c\n",n[0]);
     printf("size4:%d\n",fsize);
     printf("the string4:%s\n",n);
     fclose(ptr4);
 
-
+    fread(filename, sizeof(char), 5,fptr);
+    fread(&n[0],sizeof(char),1,fptr);
     fread(&fsize4, sizeof(int), 1, fptr);
-    fread(n, sizeof(char), fsize,fptr);
+    fread(n, sizeof(char), fsize-1,fptr);
 
-    while ((m = fgetc(fptr)) == fsize)
+    while ((m = fgetc(fptr)) == sum4)
     {
         fputc(m, ptr5);
     }
     fclose(fptr);
+    fwrite(filename, sizeof(char), 4,ptr5);
     fwrite(&n[0], sizeof(char), 1,ptr5);
     fwrite(&fsize, sizeof(int), 1, ptr5);
-    fwrite(n, sizeof(char), fsize,ptr5);
+    fwrite(n, sizeof(char), fsize-1,ptr5);
+    printf("filename5:%s\n",filename);
     printf("index5:%c\n",n[0]);
     printf("size5:%d\n",fsize);
     printf("the string5:%s\n",n);
